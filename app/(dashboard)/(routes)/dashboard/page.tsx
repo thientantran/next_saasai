45c8c3be9726ace716e6abd8b4f7a9fbeee5e4f8
thientantran <thientantm@gmail.com>
2023-07-24 10:28:45 +0700

    setup folder and page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function Dashbord() {
   return (
     <div>
-      <p className='text-6xl text-green-500'>TEST TAILWIND</p>
+      <p className='text-6xl text-green-500'>Dashboard Page</p>
       <Button variant='destructive'>Hello world</Button>
     </div>
 
