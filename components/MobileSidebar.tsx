eab70dc775ea1c5d1e29596a9fd838671f92ffde
thientantran <thientantm@gmail.com>
2023-07-24 22:37:48 +0700

    click button menu show and hide the sidebar, using shadSheet

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,11 +1,20 @@
 'use client'
+import Sidebar from '@/components/Sidebar'
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Menu } from 'lucide-react'
 
 export default function MobileSidebar() {
   return (
-    <Button variant='ghost' size='icon' className='md:hidden'>
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button variant='ghost' size='icon' className='md:hidden'>
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='left' className='p-0'>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   )
 }
