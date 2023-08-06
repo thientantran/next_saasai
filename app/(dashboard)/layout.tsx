2e76006b7849883d265b14bb7aefcdc8faba83c1
thientantran <thientantm@gmail.com>
2023-08-07 00:15:38 +0700

    create modal using dialog and hook for this Modal by using zustand
    npm i zustand
    npx shadcn-ui@latest add dialog

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ export default async function  DashboardLayyout ({ children }: { children: React
   const apiLimitCount = await getApiLimitCount()
   return (
     <div className='h-full relative'>
-      <div className="hidden h-full bg-gray-900 md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80]">
+      <div className="hidden h-full bg-gray-900 md:flex md:w-72 md:flex-col md:fixed md:inset-y-0">
         {/* chỗ này khi kích thước màn hình lớn hơn md (768px) thì có 2 cái display: none và display flex thì flex sẽ ghi đè none của hidden, nhưng khi nhỏ hơn 768px thì cái flex mất, thì cái hidden có hiệu lực, do đó display none */}
           <Sidebar apiLimitCount={apiLimitCount} />
       </div>
--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -1,6 +1,19 @@
 'use client'
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { useProModal } from "@/hooks/useProModal"
+
 export default function ProModal() {
+  const proModel = useProModal()
   return (
-    <div>ProModal</div>
+    <Dialog open={proModel.isOpen} onOpenChange={proModel.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center">
+              Upgrade to Genius
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
