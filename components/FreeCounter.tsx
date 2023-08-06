bdfb527ca805a537c4291583ef9db683809743b5
thientantran <thientantm@gmail.com>
2023-08-06 16:54:05 +0700

    layout free counter in Sidebar

--- components/FreeCounter.tsx
+++ components/FreeCounter.tsx
@@ -1,10 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import { MAX_FREE_COUNTS } from "@/constants";
-
+import { Zap } from "lucide-react";
 interface FreeCounterProps {
   apiLimitCount: number;
 }
 export default function FreeCounter({apiLimitCount=0}:FreeCounterProps) {
   return (
-    <div>FreeCounter: {apiLimitCount}/{MAX_FREE_COUNTS}</div>
+    <div className="px-3">
+      <Card className="bg-white/10 border-0">
+        <CardContent className="py-6">
+          <div className="text-center text-sm text-white mb-4 space-y-2">
+            <p>
+              {apiLimitCount}/{MAX_FREE_COUNTS} Free Generations
+            </p>
+            <Progress className="h-3" value={(apiLimitCount/MAX_FREE_COUNTS)*100}/>
+          </div>
+          <Button variant="premium" className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white"/>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
