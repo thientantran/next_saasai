29a67060e0ea888277ea59067e5c4ae240cfaa19
thientantran <thientantm@gmail.com>
2023-08-07 09:40:31 +0700

    Layout ProModal Description
    f

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -1,9 +1,44 @@
 'use client'
 
-import { Badge } from "@/components/ui/badge"
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import { useProModal } from "@/hooks/useProModal"
+import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/useProModal";
+import { cn } from "@/lib/utils";
+import { Check, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 
+export const tools = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+  },
+]
 export default function ProModal() {
   const proModel = useProModal()
   return (
@@ -16,6 +51,21 @@ export default function ProModal() {
                   <Badge className="uppercase text-sm py-1" variant='premium'>pro</Badge>
                 </div>
           </DialogTitle>
+          <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+              {tools.map((tool)=>(
+                <Card key={tool.label} className="p-3 border-black/5 flex items-center justify-between">
+                  <div className="flex items-center gap-x-4">
+                    <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                      <tool.icon className={cn('w-6 h-6', tool.color)}/>
+                    </div>
+                    <div className="font-semibold text-sm">
+                      {tool.label}
+                    </div>
+                  </div>
+                  <Check className="text-primary w-5 h-5"/>
+                </Card>
+              ))}
+          </DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
