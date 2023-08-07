4ee152961d466cb2fc0d9bf247aef94ae948748e
thientantran <thientantm@gmail.com>
2023-08-07 09:11:39 +0700

    using bagde component to add icon into promodal component

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Badge } from "@/components/ui/badge"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { useProModal } from "@/hooks/useProModal"
 
@@ -9,8 +10,11 @@ export default function ProModal() {
     <Dialog open={proModel.isOpen} onOpenChange={proModel.onClose}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle className="flex justify-center items-center">
-              Upgrade to Genius
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+                <div className="flex items-center gap-x-2 font-bold py-1">
+                  Upgrade to Genius
+                  <Badge className="uppercase text-sm py-1" variant='premium'>pro</Badge>
+                </div>
           </DialogTitle>
         </DialogHeader>
       </DialogContent>
