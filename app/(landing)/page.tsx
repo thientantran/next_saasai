edb5be7f321cf01931137df9be6eca06f04e902d
thientantran <thientantm@gmail.com>
2023-08-07 10:09:42 +0700

    redirect for user logged in

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
-
+import { redirect } from "next/navigation";
 export default function LandingPage() {
+  const { userId } = auth()
+  if(userId){
+    redirect("/dashboard")
+  }
   return (
     <div>Landing Page
       <div>
