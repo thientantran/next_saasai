30b026ce19f525a2c63a037dfe0decd8d42e0a58
thientantran <thientantm@gmail.com>
2023-08-11 15:37:21 +0700

    create settings page and check subscription on this page

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,23 @@
+import Heading from '@/components/Heading'
+import { checkSubscription } from '@/lib/subscription'
+import { Settings } from 'lucide-react'
+
+export default async function SettingPage() {
+  const isPro = await checkSubscription()
+  return (
+    <div>
+      <Heading
+        title='Settings'
+        description='Mange account settings'
+        icon={Settings}
+        iconColor='text-gray-700'
+        bgColor='text-gray-700/10'
+      />
+      <div className="px-4 space-y-4 lg:px-8">
+        <div className="text-muted-foreground text-sm">
+        {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
+        </div>
+      </div>
+    </div>
+  )
+}
