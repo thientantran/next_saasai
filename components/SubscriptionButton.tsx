84817fc0dca45c3abd62f7afd670804992022082
thientantran <thientantm@gmail.com>
2023-08-11 16:12:48 +0700

    add onClick function to redirect link to billing or checkout page
    remember active customer portal link in stripe page

--- components/SubscriptionButton.tsx
+++ components/SubscriptionButton.tsx
@@ -1,13 +1,26 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
+import axios from "axios"
 import { Zap } from "lucide-react"
 import { useState } from "react"
 
 export default function SubscriptionButton({isPro = false}: {isPro: boolean}) {
   const [loading, setLoading] = useState(false)
+  const onClick = async () => {
+    try {
+      setLoading(true)
+      const response = await axios.get("/api/stripe");
+      console.log
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log("BILLING_ERROR", error);
+    }finally{
+      setLoading(false)
+    }
+  }
   return (
-    <Button variant={isPro ? "default" : "premium"} disabled={loading}>
+    <Button variant={isPro ? "default" : "premium"} disabled={loading} onClick={onClick}>
       {isPro ? "Manage Subscription" : "Upgrade"}
       {!isPro && <Zap className="w-4 h-4 ml-2 fill-white"/>}
     </Button>
