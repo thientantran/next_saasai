cce08bee7b61c742fa77a698aeb1ab10949d38e7
thientantran <thientantm@gmail.com>
2023-08-13 16:37:10 +0700

    create layout for landingPage

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function LandingLayout({children}:{children:React.ReactNode}) {
+  return (
+    <main className="h-full overflow-auto bg-[#111827]">
+      <div className="mx-auto max-w-screen-xl h-full w-full">
+        {children}
+      </div>
+    </main>
+  )
+}
