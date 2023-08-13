80e1268105144de0a4c792df3e3f73464bb215cb
thientantran <thientantm@gmail.com>
2023-08-13 17:47:22 +0700

    edit page of Landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,25 +1,11 @@
 import LandingNavbar from "@/components/LandingNavbar";
 import LandingPageContent from "@/components/LandingPageContent";
 import LandingPageHero from "@/components/LandingPageHero";
-import { Button } from "@/components/ui/button";
 import { auth } from "@clerk/nextjs";
-import Link from "next/link";
 export default function LandingPage() {
   const { userId } = auth()
   return (
-    <div>Landing Page
-      <div>
-        <Link href='/sign-in'>
-          <Button>
-            Login
-          </Button>
-        </Link>
-        <Link href='/sign-up'>
-          <Button>
-            Register
-          </Button>
-        </Link>
-      </div>
+    <div>
       <LandingNavbar/>
       <LandingPageHero/>
       <LandingPageContent/>
